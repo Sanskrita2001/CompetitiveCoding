fe09d5065d8003b5379a00d05bd37f0cd22ae0ee
Sanskrita Saha <60420648+Sanskrita2001@users.noreply.github.com>
2021-07-05 01:55:00 +0530

    Time: 100 ms (48.28%), Space: 24.7 MB (23.54%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,73 +1,36 @@
 class Solution {
 public:
-   vector<vector<int>> threeSum(vector<int>& arr) {
-//         vector<vector<int>> res;
-//         //sorting
-//         sort(nums.begin(), nums.end());
-//         int n=nums.size();
-//         if(n<=3000){
-//             for(int i=0;i<=(n-3);i++)
-//         {
-//             if(i > 0 && nums[i] == nums[i-1])
-//                 continue;
-//             int j=i+1;
-//             int k=n-1;
-//             while(j<k){
-//                 int pair=nums[j]+nums[k];
-//             if(pair == -nums[i])
-//             {
-//                 res.push_back({nums[i],nums[j],nums[k]});
-//                 j++;
-//                 k--;
-//             }
-//             else if(pair>-nums[i])
-//                 k=k-1;
-//             else
-//                 j=j+1;
-//             } 
-//         }
-//         for (int i = 0; i < res.size(); ++i) {
-//             sort(res.begin(), res.end());
-//   res.erase(unique(res.begin(), res.end()), res.end());
-// }
-//         return res;
-//     } 
-//        return res;
-               int n = arr.size();
-
-        if(n < 3)
-            return {};
-        vector<vector<int>> res; 
-        sort(arr.begin() , arr.end());
-        
-        for(int i = 0; i < n; i++)
+   vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> res;
+        //sorting
+        sort(nums.begin(), nums.end());
+        int n=nums.size();
+            for(int i=0;i<=(n-3);i++)
         {
-            if(i > 0 && arr[i] == arr[i-1])
+            if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            
-            int j = i+1, k = n-1;
-            
-            while(j < k)
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int pair=nums[j]+nums[k];
+            if(pair == -nums[i])
             {
-                int sum = arr[i] + arr[j] + arr[k];
-                if(sum < 0)
-                    j++;
-                else if(sum > 0)
-                    k--;
-                else
-                {
-                    res.push_back({arr[i] , arr[j] , arr[k]});
-                    
-                    while(j < k && arr[j] == arr[j+1]) j++;
-                    while(j < k && arr[k] == arr[k-1]) k--;
-                    j++;
-                    k--;
-                }
-            }
+                res.insert({nums[i],nums[j],nums[k]});
                 
+                // while(j < k && nums[j] == nums[j+1]) j++;
+                // while(j < k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
+            }
+            else if(pair>-nums[i])
+                k=k-1;
+            else
+                j=j+1;
+            } 
         }
-        return res;
-       
-}
-        
+ vector<vector<int>>solve(res.begin(),res.end());
+   return solve;
+       return solve;
+
+        }     
 };
