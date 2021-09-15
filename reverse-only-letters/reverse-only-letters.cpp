b565a13d3569ca29b1084d5e48b38972b45b8ec5
Sanskrita Saha <60420648+Sanskrita2001@users.noreply.github.com>
2021-09-15 11:49:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.49%) - LeetHub

---
+++ reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int n = s.length();
+        int i=0,j=n-1;
+        while(i<=j){
+            if(isalpha(s[i])&&isalpha(s[j]))
+                swap(s[i++],s[j--]);
+            else if(!isalpha(s[i])){
+               i++; 
+            }  
+            else{
+                j--;
+            }
+        }
+        return s;
+    }
+};
