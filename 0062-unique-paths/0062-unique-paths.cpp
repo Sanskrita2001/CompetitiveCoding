d4e0dd0d14878db7bf585520594da34b9220c362
Sanskrita Saha <60420648+Sanskrita2001@users.noreply.github.com>
2022-12-01 18:54:28 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (69.01%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,18 +13,19 @@ public:
         return dp[i][j]=right+down;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp (m,vector<int> (n,-1));
-        dp[m-1][n-1]=1;
+        vector<vector<int>> dp (2,vector<int> (n,1));
+        dp[1][n-1]=1;
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 int right=0,down=0;
                 if(i==m-1 && j==n-1)
                     continue;
                 if(i+1<m)
-                    right = dp[i+1][j];
+                    right = dp[1][j];
                 if(j+1<n)
-                    down = dp[i][j+1];
-                dp[i][j]=right+down;
+                    down = dp[0][j+1];
+                dp[0][j]=right+down;
+                dp[1]=dp[0];
             }
         }
         return dp[0][0];
